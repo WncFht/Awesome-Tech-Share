3acc07dec90acec93c5081a8b4858b56a04cf8c4
KinnariyaMamaTanha <2823324228@qq.com>
2025-02-27 17:27:37 +0800

    chore: remove unused CSS and JavaScript files, and redundant plugins, update README and requirements

--- README.md
+++ README.md
@@ -7,10 +7,6 @@ This is a repo for the collection of any awesome resources and reflection.
 ```shell
 conda create -f environment.yml
 
-git clone https://github.com/TonyCrane/mkdocs-toolchain
-pip install mkdocs-toolchain/mkdocs-toc-plugin
-rm -rf mkdocs-toolchain
-
 git clone https://github.com/KinnariyaMamaTanha/mkdocs-statistics-plugin
 pip install ./mkdocs-statistics-plugin
 rm -rf mkdocs-statistics-plugin
--- overrides/js/mathjax.js
+++ overrides/js/mathjax.js
@@ -1,16 +1,19 @@
 window.MathJax = {
-    tex: {
-      inlineMath: [["\\(", "\\)"]],           // Inline math delimiters
-      displayMath: [["\\[", "\\]"]],          // Display math delimiters
-      processEscapes: true,                    // Handle escaped characters
-      processEnvironments: true                // Process LaTeX environments
-    },
-    options: {
-      ignoreHtmlClass: ".*|",                 // Ignore all classes by default
-      processHtmlClass: "arithmatex"          // Only process elements with this class
-    }
-  };
-  
-  document$.subscribe(() => { 
-    MathJax.typesetPromise()                  // Render math when document loads
-  })
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
